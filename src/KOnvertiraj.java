00a39943fb69cee11eae3ca9810ab1347cbf4e1e
Azra Ademovic <azra,ademovic96master@outlook.com>
2018-10-17 16:38:18 +0200

    Univerzalni Konverter

---
+++ src/KOnvertiraj.java
@@ -0,0 +1,29 @@
+
+public class KOnvertiraj {
+	public double VratiKilogramUFunte(double kolicina) {
+		return 0; }
+	
+	
+
+	public double VratiFuntiUkg(double kolicina) {
+		return 0;
+
+	}
+
+	public double VratiIncUcm(double kolicina) {
+		return 0;
+
+	}
+
+	public double VratiFuCelzijuse(double kolicina) {
+		return 0;
+
+	}
+
+	public double VratiCelzijuseuF(double kolicina) {
+		return 0;
+
+	}
+
+
+}
